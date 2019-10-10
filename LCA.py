f71eeaf318378fcceb059f677d21587c42416a75
unknown <odonner9@tcd.ie>
2019-10-10 11:11:41 +0100

    Added unit tests for unique tree types

--- LCA.py
+++ LCA.py
@@ -57,26 +57,83 @@ def findLCA(root, n1, n2):
         i += 1
     return path1[i-1] 
   
-  
-# Driver program to test above function 
-# Create binary tree 
-root = Node(1) 
-root.left = Node(2) 
-root.right = Node(3) 
-root.left.left = Node(4) 
-root.left.right = Node(5) 
-root.right.left = Node(6) 
-root.right.right = Node(7) 
-  
-# print ("LCA(4, 5) = " , findLCA(root, 4, 5)) 
-# print ("LCA(4, 6) = " , findLCA(root, 4, 6)) 
-# print ("LCA(3, 4) = " , findLCA(root, 3, 4)) 
-# print ("LCA(2, 4) = " , findLCA(root, 2, 4)) 
+ 
 
 class TestLCA(unittest.TestCase):
 
 
  def test_findLCA(self):
+
+        # Empty Tree
+        root = None
+
+        expectedOutput = -1
+        actualOutput = findLCA(root, 0, 0)
+        self.assertEqual(actualOutput, expectedOutput)
+
+        expectedOutput = -1
+        actualOutput = findLCA(root, -4, 5)
+        self.assertEqual(actualOutput, expectedOutput)
+
+
+        # One Node Tree
+        root = Node(1)
+
+        expectedOutput = 1
+        actualOutput = findLCA(root, 1, 1)
+        self.assertEqual(actualOutput, expectedOutput)
+
+        expectedOutput = -1
+        actualOutput = findLCA(root, 0, 0)
+        self.assertEqual(actualOutput, expectedOutput)        
+
+        expectedOutput = -1
+        actualOutput = findLCA(root, 1, 3)
+        self.assertEqual(actualOutput, expectedOutput) 
+
+        expectedOutput = -1
+        actualOutput = findLCA(root, 4, 5)
+        self.assertEqual(actualOutput, expectedOutput)               
+
+
+        # One sided tree
+        root = Node(1)
+        root.left = Node(2)
+        root.left.left = Node(3)
+        root.left.left.left = Node(4)
+
+        expectedOutput = 3
+        actualOutput = findLCA(root, 3, 3)
+        self.assertEqual(actualOutput, expectedOutput)
+
+        expectedOutput = 2
+        actualOutput = findLCA(root, 3, 2)
+        self.assertEqual(actualOutput, expectedOutput)
+
+        expectedOutput = 1
+        actualOutput = findLCA(root, 4, 1)
+        self.assertEqual(actualOutput, expectedOutput)
+
+        expectedOutput = -1
+        actualOutput = findLCA(root, 1, 0)
+        self.assertEqual(actualOutput, expectedOutput)
+
+        expectedOutput = -1
+        actualOutput = findLCA(root, 7, -5)
+        self.assertEqual(actualOutput, expectedOutput)
+
+        # clear tree for next tests
+        root = None
+
+
+        # Typical tree
+        root = Node(1) 
+        root.left = Node(2) 
+        root.right = Node(3) 
+        root.left.left = Node(4) 
+        root.left.right = Node(5) 
+        root.right.left = Node(6) 
+        root.right.right = Node(7) 
 
         # basic cases 
         expectedOutput = 2
@@ -119,7 +176,7 @@ class TestLCA(unittest.TestCase):
 
         # example where two nodes aren't in tree
         expectedOutput = -1
-        actualOutput = findLCA(root, 9, -7)
+        actualOutput = findLCA(root, 9, 12)
         self.assertEqual(actualOutput, expectedOutput)
 
         # example where a node given is negative
